b872f3d51d52f444b1e0228637fccb3f91a13364
배성환 <baeseongwhan@ulim.co>
2025-09-02 09:59:58 +0900

    프로젝트 초기세팅

---
+++ chat-server-api/src/main/java/com/chatserverapi/ChatServerApiApplication.java
@@ -0,0 +1,13 @@
+package com.chatserverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatServerApiApplication.class, args);
+	}
+
+}
---
+++ chat-server-api/src/test/java/com/chatserverapi/ChatServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatserverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
