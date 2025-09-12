d27b6f081f1802a6a82c5f2507c38496662df63a
배성환 <baeseongwhan@ulim.co>
2025-09-12 13:26:02 +0900

    프론트 다시세팅

--- chat-server-api/src/main/java/com/chatserverapi/common/configs/SecurityConfigs.java
+++ chat-server-api/src/main/java/com/chatserverapi/common/configs/SecurityConfigs.java
@@ -48,7 +48,7 @@ public class SecurityConfigs {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("localhost:5173"));
+        corsConfiguration.setAllowedOrigins(List.of("localhost:3000"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setExposedHeaders(List.of("Set-Cookie", "Content-Disposition"));
